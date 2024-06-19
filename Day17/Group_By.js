f0d5c6e69d2b82f4def2e1d483ffb2a1f46767ad
prashantpandey05 <prashantpandey1543@gmail.com>
2024-06-19 19:55:28 +0530

    Day17

---
+++ Day17/Group_By.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/group-by
+
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((grouped, item) => {
+      const key = fn(item);
+      
+      if (!grouped[key]) {
+        grouped[key] = [];
+      }
+      
+      grouped[key].push(item);
+      
+      return grouped;
+    }, {});
+  };
+  
