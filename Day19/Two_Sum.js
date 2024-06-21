5574a4f3823f5c7cb458215d99c243986024c43c
Prashant Pandey <109054065+prashantpandey05@users.noreply.github.com>
2024-06-21 21:30:49 +0530

    Day19 Solution

---
+++ Day19/Two_Sum.js
@@ -0,0 +1,13 @@
+//  https://leetcode.com/problems/two-sum/
+
+var twoSum = function(nums, target) {
+    const pairIdx = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (target - num in pairIdx) {
+            return [i, pairIdx[target - num]];
+        }
+        pairIdx[num] = i;
+    }    
+};
