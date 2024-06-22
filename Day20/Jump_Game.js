bd5ece7a02019935912413a0cc108e13dff2c4c7
Prashant Pandey <109054065+prashantpandey05@users.noreply.github.com>
2024-06-23 01:30:01 +0530

    Day20

---
+++ Day20/Jump_Game.js
@@ -0,0 +1,13 @@
+//  https://leetcode.com/problems/jump-game/description/
+
+var canJump = function(nums) {
+    let goal = nums.length - 1;
+
+    for (let i = nums.length - 2; i >= 0; i--) {
+        if (i + nums[i] >= goal) {
+            goal = i;
+        }
+    }
+
+    return goal === 0;    
+};
