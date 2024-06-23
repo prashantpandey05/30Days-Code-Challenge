800d2f6307ee7b1b8d501b337989ff7d4e6bbfbc
Prashant Pandey <109054065+prashantpandey05@users.noreply.github.com>
2024-06-23 22:32:55 +0530

    Day21

--- Day21/Best_Time_to_Buy_and_Sell_Stock.js
+++ Day21/Best_Time_to_Buy_and_Sell_Stock.js
@@ -1,16 +1,18 @@
 //  https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-class Solution {
-    maxProfit(prices) {
-        let buy = prices[0];
-        let profit = 0;
-        for (let i = 1; i < prices.length; i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else if (prices[i] - buy > profit) {
-                profit = prices[i] - buy;
-            }
-        }
-        return profit;
+const maxProfit = (prices) => {
+  let left = 0; // Buy
+  let right = 1; // sell
+  let max_profit = 0;
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left]; // our current profit
+
+      max_profit = Math.max(max_profit, profit);
+    } else {
+      left = right;
     }
-}
+    right++;
+  }
+  return max_profit;
+};
