8c11c07116c0a9af618077c70ef3e3c1b427421c
Prashant Pandey <109054065+prashantpandey05@users.noreply.github.com>
2024-06-26 21:26:51 +0530

    Day2

---
+++ Day24/String_to_Integer_(atoi).js
@@ -0,0 +1,11 @@
+//  https://leetcode.com/problems/string-to-integer-atoi/
+
+let myAtoi = function(s) {
+  const ans = Number.parseInt(s);
+     if (ans) {
+      if (ans <= -2147483648)return -2147483648;
+      else if (ans >= 2147483647)return 2147483647;
+      else return ans;
+  }
+  return 0;
+};
