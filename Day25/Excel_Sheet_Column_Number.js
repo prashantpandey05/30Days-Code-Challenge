99dd1c5b0788a6058fbcadeb4f3d3b4abc2ec4bc
Prashant Pandey <109054065+prashantpandey05@users.noreply.github.com>
2024-06-27 19:27:04 +0530

    Day25

---
+++ Day25/Excel_Sheet_Column_Number.js
@@ -0,0 +1,14 @@
+//  https://leetcode.com/problems/excel-sheet-column-number/
+
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    let result = 0;
+    let length = columnTitle.length;
+    for(let i = 0; i < length; i++){
+        result += (columnTitle.charCodeAt(i) - 64) * Math.pow(26, length - (i + 1));
+    }
+    return result;
+};
